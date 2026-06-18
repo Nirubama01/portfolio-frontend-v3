937d4d46f4eb3b90f41ffb7fa92842470cad20f6
Nirubama01 <nirubama9787@gmail.com>
2026-06-18 10:50:05 +0530

    commit1

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1,8 +1,47 @@
+import { useEffect, useState } from "react";
+
 function AdminDashboard() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://x5xv9nqfag.execute-api.ap-south-1.amazonaws.com/prod/portfolio/users"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        const usersData = JSON.parse(data.body);
+        setUsers(usersData);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
   return (
-    <div>
+    <div style={{ padding: "20px" }}>
       <h1>Admin Dashboard</h1>
-      <p>Welcome Admin</p>
+
+      <h2>All Users</h2>
+
+      <table border="1" cellPadding="10">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>User Id</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.userId}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>{user.userId}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
